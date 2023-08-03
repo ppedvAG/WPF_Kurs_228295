90cf2857617d0c3550252328511bd0ba7aac6db5
KlaasHagge <44665897+KlaasHagge@users.noreply.github.com>
2023-08-03 12:06:46 +0200

    Lab10_Trigger

---
+++ Trigger/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Trigger
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public bool BoolVal { get; set; }
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.BoolVal = true;
+
+            //Setzen des DataContext
+            this.DataContext = this;
+        }
+
+        //EventHandler zum Ändern der Property
+        private void Btn_Ändern_Click(object sender, RoutedEventArgs e)
+        {
+            BoolVal = !BoolVal;
+
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(BoolVal)));
+        }
+    }
+}
